1a0f733c97dce990ab96d2a46cf746e9ca23463d
JuanMendozaOS <juanmendozasfa@gmail.com>
2023-08-20 22:42:54 -0400

    agregado archivo con métodos de path

---
+++ 5.path.js
@@ -0,0 +1,19 @@
+const path = require('node:path');
+
+// unir rutas con path.join
+// -> unix /
+// -> windows \
+
+console.log(path.sep);
+
+
+const filePath  = path.join('./content', 'subfolder', 'test.txt');
+
+const base = path.basename('/carpeta/subcarpeta/archivo.txt');
+
+console.log(base);
+
+const filename = path.basename('/carpeta/subcarpeta/archivo.txt', '.txt');
+
+console.log(filename);
+
