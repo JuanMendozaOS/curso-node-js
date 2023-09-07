86c789adc862873c0a4ed0c5c9829d3854a139db
JuanMendozaOS <juanmendozasfa@gmail.com>
2023-09-06 21:03:23 -0400

    Delete movies by id - Added CORS middleware using npm cors

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,19 +1,36 @@
 const express = require('express')
 const crypto = require('node:crypto')
 const movies = require('./movies.json')
+const cors = require('cors')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 const app = express()
 app.disable('x-powered-by')
 
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hola mundo' })
 })
 
 app.get('/movies', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -61,7 +78,9 @@ app.patch('/movies/:id', (req, res) => {
   // usamos el index hasta que manejemos la info en base de datos
   const movieIndex = movies.findIndex(movie => movie.id === id)
 
-  if (movieIndex < 0) return res.status(404).json({ status: 404, errorMessage: 'Movie not found' })
+  if (movieIndex < 0) {
+    return res.status(404).json({ status: 404, errorMessage: 'Movie not found' })
+  }
 
   const updatedMovie = {
     ...movies[movieIndex],
@@ -73,7 +92,21 @@ app.patch('/movies/:id', (req, res) => {
   res.status(201).json(updatedMovie)
 })
 
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex < 0) {
+    return res.status(404).json({ status: 404, errorMessage: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  res.json({ message: 'Movie deleted' })
+})
+
 const PORT = process.env.PORT ?? 1234
+
 app.listen(PORT, (req, res) => {
   console.log(`Listening on port http://localhost:${PORT}`)
 })
